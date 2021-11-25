cd5e071c41f3dbd50dbc34b3206d1667c90a68ab
Diego Soares <61186064+osdiego@users.noreply.github.com>
2021-11-25 21:28:10 +0000

    Adding first version of files

---
+++ sudoku/ac3.py
@@ -0,0 +1,47 @@
+from utils import is_different
+
+
+def AC3(csp, queue=None):
+    """
+    Constraint Propagation with AC-3
+    """
+    if queue == None:
+        queue = list(csp.binary_constraints)
+
+    while queue:
+
+        (xi, xj) = queue.pop(0)
+
+        if revise(csp, xi, xj):
+
+            # if a cell has 0 possibilities, sudoku has no solution
+            if len(csp.possibilities[xi]) == 0:
+                return False
+
+            for Xk in csp.related_cells[xi]:
+                if Xk != xi:
+                    queue.append((Xk, xi))
+
+    return True
+
+
+def revise(csp, cell_i, cell_j):
+    """
+    remove_inconsistent_values
+
+    returns true if a value is removed
+    """
+    removed = False
+
+    # for each possible value remaining for the cell_i cell
+    for value in csp.possibilities[cell_i]:
+
+        # if cell_i=value is in conflict with cell_j=poss for each possibility
+        if not any([is_different(value, poss) for poss in csp.possibilities[cell_j]]):
+
+            # then remove cell_i=value
+            csp.possibilities[cell_i].remove(value)
+            removed = True
+
+    # returns true if a value has been removed
+    return removed
