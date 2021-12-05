ab0f3aeae06957f97781d3da352bbc8a01a7b379
Diego Soares <61186064+osdiego@users.noreply.github.com>
2021-12-05 18:25:15 +0000

    Update naive_bayes.py

--- spam_filter/naive_bayes.py
+++ spam_filter/naive_bayes.py
@@ -5,19 +5,19 @@ class NaiveBayes:
     def __init__(self, c: float = 0.2) -> None:
         self.c = c
         self.b = 0
-        # self.bag_of_words initialized in train method
+        # self.all_document_words initialized in train method
         self.n = 0  # number of distinct words in all train documents
         # self.p initialized in train method
 
     def train(self, documents: list[list[str]], labels: list[str]) -> None:
-        self.bag_of_words = set()
+        self.all_document_words = set()
 
         for document in documents:
             for word in document:
-                self.bag_of_words.add(word)
+                self.all_document_words.add(word)
 
-        self.bag_of_words = tuple(sorted(list(self.bag_of_words)))
-        self.n = len(self.bag_of_words)
+        self.all_document_words = tuple(sorted(list(self.all_document_words)))
+        self.n = len(self.all_document_words)
 
         m_ham = labels.count('ham')
         m_spam = labels.count('spam')
@@ -25,8 +25,8 @@ class NaiveBayes:
         self.b = log(self.c) + log(m_ham) - log(m_spam)
 
         self.p = {
-            'spam': dict(zip(self.bag_of_words, (1 for i in range(self.n)))),
-            'ham': dict(zip(self.bag_of_words, (1 for i in range(self.n))))
+            'spam': dict(zip(self.all_document_words, (1 for i in range(self.n)))),
+            'ham': dict(zip(self.all_document_words, (1 for i in range(self.n))))
         }
 
         w_spam = self.n
@@ -52,7 +52,7 @@ class NaiveBayes:
         t = -self.b
 
         for word in set(document):
-            if word in self.bag_of_words:
+            if word in self.all_document_words:
                 t += document.count(word) * \
                     (log(self.p['spam'][word]) - log(self.p['ham'][word]))
 
