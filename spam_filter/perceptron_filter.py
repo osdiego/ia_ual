e59d724e23871cf72c445dd104ad5e241bb5334e
Diego Soares <61186064+osdiego@users.noreply.github.com>
2021-12-07 21:36:47 +0000

    Update perceptron_filter.py

--- spam_filter/perceptron_filter.py
+++ spam_filter/perceptron_filter.py
@@ -1,19 +1,48 @@
-
-def perceptron(todos os vetores dos documentos, todas as labels, n de documentos, épocas):
-    teta = vetor a 0 com n de palavras do lexico
-    teta0 = 0
-
-    for epoca in epocas:
-        for documento in documentos:
-            if label do documento * ((teta * vetor de frequencias do documento) + teta0) <= 0:
-                teta = teta + (label do documento * vetor de frequencias do documento)
-                teta0 = teta0 + label do documento
-
-    return teta, teta0
+from numpy import dot
 
 
-def classificador(vetor de frequencias do documento, teta, teta0):
-    if (teta * vetor de frequencias do documento) + teta0 <= 0:
-        return 'spam'
-    else:
-        return 'ham'
+class Perceptron:
+    def __init__(self, documents: list[list[str]], labels: list[str], t: int = 10) -> None:
+        self.n = len(documents)
+        self.documents = documents
+        self.labels = labels
+        self.t = t
+        self.prepare()
+
+    def prepare(self) -> None:
+        # Preparing lexicon
+        self.all_document_words = set()
+
+        for document in self.documents:
+            for word in document:
+                self.all_document_words.add(word)
+
+        self.all_document_words = tuple(sorted(list(self.all_document_words)))
+
+        # Preparing document vectors
+        self.all_document_vectors = []
+
+        for document in self.documents:
+            self.all_document_vectors.append(
+                [document.count(word) for word in self.all_document_words]
+            )
+
+    def train(self):
+        self.theta = [0 for i in self.n]
+        self.theta_0 = 0
+
+        for _ in self.t:
+            for vector, label in zip(self.all_document_vectors, self.labels):
+                if label * (dot(self.theta, vector) + self.theta_0) <= 0:
+                    label_x_vector = [label * v for v in vector]
+                    self.theta = [
+                        x + y for x, y in zip(self.theta, label_x_vector)
+                    ]
+                    self.theta_0 += label
+
+    def classifier(self, document: list[str], label: int):
+        if dot(self.theta, vector) + self.theta_0 <= 0:
+        if (teta * vetor de frequencias do documento) + teta0 <= 0:
+            return 'spam'
+        else:
+            return 'ham'
